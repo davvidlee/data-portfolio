cb3646a65dc3d827c9d624edab3c3966589fa122
David Lee <davidlee092802@gmail.com>
2025-04-13 23:48:15 -0700

    package lock update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  output: "export",
-  basePath: isProd ? "/data-portfolio" : "",
-  assetPrefix: isProd ? "/data-portfolio/" : "",
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/your-repo-name' : '',
+  assetPrefix: isProd ? '/your-repo-name/' : '',
 };
+
+module.exports = nextConfig;
