a869e46450575fb44e0cf7e8e128be2877c6e09a
David Lee <davidlee092802@gmail.com>
2025-04-14 00:06:11 -0700

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/data-portfolio' : '',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
   assetPrefix: isProd ? '/data-portfolio/' : '',
+  basePath: isProd ? '/data-portfolio' : '',
+  output: 'export'
 };
 
-module.exports = nextConfig;
+export default nextConfig;
