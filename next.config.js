df737458aad6f9c44d06dfbcd0937cedd3479e72
David Lee <davidlee092802@gmail.com>
2025-04-13 23:53:02 -0700

    fixed typos

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/your-repo-name' : '',
-  assetPrefix: isProd ? '/your-repo-name/' : '',
+  basePath: isProd ? '/data-portfolio' : '',
+  assetPrefix: isProd ? '/data-portfolio/' : '',
 };
 
 module.exports = nextConfig;
